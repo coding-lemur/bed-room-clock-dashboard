541fdf6581d9b7bfef0381c58e4ff5e817ee4afa
René Schimmelpfennig <mail@reneschimmelpfennig.de>
2022-08-14 14:48:53 +0200

    show some values

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,64 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1 class="mb-3">Bedroom Clock</h1>
+
+    <section id="device-info" class="mb-3">
+      <h2>Device Info</h2>
+
+      <div>firmware version: <span class="value">{{ version }}</span></div>
+      <div>device ID: <strong class="value">{{ deviceId }}</strong></div>
+
+      <div>
+        wifi signal:
+        <span class="value">{{ wifiQuality }}</span>
+      </div>
+      <div>time: <span class="value">n.a.</span></div>
+      <div>
+        uptime:
+        <span class="value">{{ uptime }}</span>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import humanizeDuration from 'humanize-duration'
+//import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    //HelloWorld
+  },
+  data() {
+    return {
+      version: undefined,
+      deviceId: undefined,
+      wifiQuality: undefined,
+      uptime: undefined,
+      timer: undefined,
+    }
+  },
+  methods: {
+    async loadData() {
+      const resp = await fetch('/api/info')
+      const data = await resp.json()
+      const { system, network } = data
+
+      this.version = data.version
+      this.deviceId = system.deviceId
+      this.wifiQuality = network.wifiQuality
+      this.uptime = humanizeDuration(system.uptime * 1000)
+    },
+    beforeMount() {
+      this.timer = setInterval(this.loadData, 30000)
+    },
+    beforeUnmount() {
+      clearInterval(this.timer);
+    },
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
