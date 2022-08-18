3f6d17dd4baf6b350193dc2a7f0b82a49b6044e3
René Schimmelpfennig <mail@reneschimmelpfennig.de>
2022-08-18 21:49:01 +0200

    Development (#1)
    * add settings (unfinished); add hard-reset action
    
    * implement load brightness and save settings

--- src/App.vue
+++ src/App.vue
@@ -37,7 +37,20 @@
       </ul>
     </section>
 
+    <section id="settings">
+      <h2>Settings</h2>
 
+      <label for="brightness" class="form-label">brightness</label>
+      <input type="range" class="form-range" id="brightness" min="0" max="255" :value="brightness"
+        @input="e => this.brightness = e.target.value">
+    </section>
+
+    <section id="admin-actions" class="mb-3">
+      <h2>Admin Stuff</h2>
+
+      <button type="button" class="btn btn-primary" @click="saveSettings">Save Settings</button>
+      <button type="button" class="btn btn-danger ms-2" @click="hardReset">Hard Reset</button>
+    </section>
   </div>
 </template>
 
@@ -61,7 +74,10 @@ export default {
 
       // timers
       dataTimer: undefined,
-      uptimeTimer: undefined,
+      refreshTimer: undefined,
+
+      // settings
+      brightness: undefined,
     }
   },
   computed: {
@@ -70,7 +86,7 @@ export default {
     },
   },
   methods: {
-    async loadData() {
+    async loadInfo() {
       const resp = await fetch('/api/info')
       const data = await resp.json()
       const { system, network, values } = data
@@ -92,22 +108,53 @@ export default {
       this.temperature = values.temp
       this.humidity = values.humidity
     },
-    updateUptime() {
+    async loadSettings() {
+      const resp = await fetch("/api/settings")
+
+      if (!resp.ok) {
+        return
+      }
+
+      const settings = await resp.json()
+      this.brightness = settings.brightness
+    },
+    async saveSettings() {
+      const payload = { brightness: this.brightness }
+      const resp = await fetch("/api/settings", {
+        method: "POST", headers: {
+          'Content-Type': 'application/json'
+        }, body: JSON.stringify(payload)
+      })
+
+      if (!resp.ok) {
+        console.error('save settings fail')
+      }
+    },
+    refreshUptime() {
       if (!this.uptime) {
         return
       }
 
       this.uptime++
-    }
+    },
+    async hardReset() {
+      // TODO show confirmation dialog
+      const resp = await fetch('/api/hard-reset', { method: 'POST' })
+
+      if (!resp.ok) {
+        console.error('hard-reset fail')
+      }
+    },
   },
   beforeMount() {
-    this.dataTimer = setInterval(async () => { await this.loadData() }, 30000)
-    this.uptimeTimer = setInterval(this.updateUptime, 1000)
+    this.dataTimer = setInterval(async () => { await this.loadInfo() }, 30000)
+    this.refreshTimer = setInterval(this.refreshUptime, 1000)
 
-    this.loadData()
+    this.loadInfo()
+    this.loadSettings()
   },
   beforeUnmount() {
-    clearInterval(this.uptimeTimer)
+    clearInterval(this.refreshTimer)
     clearInterval(this.dataTimer)
   }
 }
