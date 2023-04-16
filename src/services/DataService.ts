f267ce918ba4a5baf7e27e38d985a128bf085d31
René Schimmelpfennig <mail@reneschimmelpfennig.de>
2023-04-16 20:59:20 +0200

    add service

---
+++ src/services/DataService.ts
@@ -0,0 +1,1 @@
+export const loadInfo = () => fetch('http://192.168.178.83/api/info')
---
+++ src/services/types/Info.ts
@@ -0,0 +1,32 @@
+type Info = {
+    version: string
+    system: {
+        deviceId: string
+        freeHeap: number
+        uptime: number
+        time: number
+    }
+    fileSystem: {
+        totalBytes: number
+        usedBytes: number
+    }
+    network: {
+        wifiRssi: number
+        wifiQuality: number
+        wifiSsid: string
+        id: string
+        mac: string
+    }
+    player: {
+        isPlaying: boolean
+        codec: string
+        bitrate: number
+        volume: number
+    }
+    values: {
+        temp: number
+        humidity: number
+    }
+}
+
+export default Info
