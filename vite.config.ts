b38bb8fa7ac6bdf26e78be764f0882d025b39f65
René Schimmelpfennig <mail@reneschimmelpfennig.de>
2023-04-19 20:59:30 +0200

    configure API proxy for DEV server

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://192.168.178.83/api',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    }
+  }
 })
