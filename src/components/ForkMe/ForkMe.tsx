b952d33677c0d5fb86ecf47272c0d50478038f62
René Schimmelpfennig <mail@reneschimmelpfennig.de>
2023-04-19 21:34:08 +0200

    add ForkMe badge

---
+++ src/components/ForkMe/ForkMe.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react'
+
+import './Style.css'
+
+const ForkMe: FC = () => {
+    return <span id="forkongithub"><a href="https://github.com/coding-lemur/bed-room-clock-dashboard">Fork me on GitHub</a></span>
+}
+
+export default ForkMe
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+import ForkMe from './components/ForkMe/ForkMe';
 import App from './App'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <ForkMe />
     <App />
   </React.StrictMode>,
 )
