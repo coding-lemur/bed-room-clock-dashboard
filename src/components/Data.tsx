13e1a7a3d10177556d379c32c30818a846191f57
René Schimmelpfennig <mail@reneschimmelpfennig.de>
2023-04-23 17:29:33 +0200

    add unit prop

--- src/components/Data.tsx
+++ src/components/Data.tsx
@@ -1,15 +1,22 @@
 
 import { FC } from 'react'
+
 import { IconProp } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 type ValueProps = {
     icon: IconProp
     value: string | number | undefined
+    unit?: string
 }
 
-const Data: FC<ValueProps> = ({ icon, value }) => {
-    return <><FontAwesomeIcon icon={icon} /> {value ?? 'n.a.'}</>
+const Data: FC<ValueProps> = ({ icon, value, unit }) => {
+    return <>
+        <FontAwesomeIcon icon={icon} /> {value ?? 'n.a.'}
+        {value !== undefined &&
+            <>{unit}</>
+        }
+    </>
 }
 
 export default Data
