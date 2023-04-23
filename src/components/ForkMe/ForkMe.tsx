15fd6cb19ff1ab080975b3e6f8e258750fbf42b4
René Schimmelpfennig <mail@reneschimmelpfennig.de>
2023-04-23 17:17:26 +0200

    remove FormMe badge

--- src/components/ForkMe/ForkMe.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from 'react'
-
-import './Style.css'
-
-const ForkMe: FC = () => {
-    return <span id="forkongithub"><a href="https://github.com/coding-lemur/bed-room-clock-dashboard">Fork me on GitHub</a></span>
-}
-
-export default ForkMe
