681878210a31a0f451a21bae18e6e98daa9d83e1
René Schimmelpfennig <mail@reneschimmelpfennig.de>
2023-04-26 20:43:00 +0200

    add all setting fields; add button-group

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -3,6 +3,7 @@ import { FC, useState, useCallback, useEffect, ChangeEvent } from 'react';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
 import { Settings as SettingsType } from '../services/types';
 import { loadSettings } from '../services/DataService';
@@ -23,25 +24,47 @@ const Settings: FC<SettingsProps> = ({ }) => {
         loadData()
     }, [])
 
-    const handleBrightnessChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-        console.log('🦘', e.target.value)
-
-        setSettings(oldState => ({
+    const handleBrightnessChange = useCallback((e: ChangeEvent<HTMLInputElement>) =>
+        setSettings(oldState => oldState ? ({
+            ...oldState,
             brightness: Number.parseInt(e.target.value),
-            screenOnDistance: oldState?.screenOnDistance ?? 0,
-            screenOnInterval: oldState?.screenOnInterval ?? 0,
-        }))
-    }, [])
+        }) : null), [])
+
+    const handleScreenOnDistanceChange = useCallback((e: ChangeEvent<HTMLInputElement>) =>
+        setSettings(oldState => oldState ? ({
+            ...oldState,
+            screenOnDistance: Number.parseInt(e.target.value),
+        }) : null), [])
+
+    const handleScreenOnIntervalChange = useCallback((e: ChangeEvent<HTMLInputElement>) =>
+        setSettings(oldState => oldState ? ({
+            ...oldState,
+            screenOnInterval: Number.parseInt(e.target.value),
+        }) : null), [])
 
     return <section id='settings' className='mb-3'>
         <h2>Settings</h2>
         <InputGroup className='mb-3'>
-            <InputGroup.Text>Brightness</InputGroup.Text>
-            <Form.Control type='number' min={0} max={255} aria-label="brightness" value={settings?.brightness} onChange={handleBrightnessChange} />
+            <InputGroup.Text>Display Brightness</InputGroup.Text>
+            <Form.Control type='number' min={0} max={255} aria-label="display brightness" value={settings?.brightness} onChange={handleBrightnessChange} />
         </InputGroup>
 
-        <Button variant="primary">Save Settings</Button>{' '}
-        <Button variant="danger">Factory Reset</Button>{' '}
+        <InputGroup className='mb-3'>
+            <InputGroup.Text>Screen On Distance</InputGroup.Text>
+            <Form.Control type='number' min={0} max={255} aria-label="screen on distance" value={settings?.screenOnDistance} onChange={handleScreenOnDistanceChange} />
+            <InputGroup.Text>cm</InputGroup.Text>
+        </InputGroup>
+
+        <InputGroup className='mb-3'>
+            <InputGroup.Text>Screen On Interval</InputGroup.Text>
+            <Form.Control type='number' min={0} max={255} aria-label="screen on interval" value={settings?.screenOnInterval} onChange={handleScreenOnIntervalChange} />
+            <InputGroup.Text>ms</InputGroup.Text>
+        </InputGroup>
+
+        <ButtonGroup aria-label="Basic example">
+            <Button variant="primary">Save Settings</Button>
+            <Button variant="danger">Factory Reset</Button>
+        </ButtonGroup>
     </section>
 }
 
