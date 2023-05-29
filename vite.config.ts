14e465ec20a59a1de4491dcb20d39d68af90fb6d
René Schimmelpfennig <mail@reneschimmelpfennig.de>
2023-05-29 17:14:08 +0200

    try to fix deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://192.168.178.83/api',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+export default defineConfig(({ command }) => {
+  const baseConfig = {
+    plugins: [react()],
+  }
+
+  if (command === 'serve') {
+    return {
+      ...baseConfig,
+      server: {
+        proxy: {
+          '/api': {
+            target: 'http://192.168.178.83/api',
+            changeOrigin: true,
+            rewrite: (path) => path.replace(/^\/api/, '')
+          }
+        }
       }
     }
+  }
+
+  // command === 'build'
+  return {
+    ...baseConfig,
+    base: '/bed-room-clock-dashboard/'
   }
 })
